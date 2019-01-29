083b14b13fe5c9dbb63fa33a45853cbac6c440b0
Thomas <bocquet.t@gmail.com>
2019-01-29 19:27:14 +0100

    dbg prebuild.sh

--- CI/prebuild.sh
+++ CI/prebuild.sh
@@ -2,4 +2,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir $SCRIPTPATH/../packages
 ln -s ${unityPath}/Editor/Data/Managed/UnityEngine.dll $SCRIPTPATH/../packages
+ls -la $SCRIPTPATH/../packages
 nuget restore $SCRIPTPATH/../
