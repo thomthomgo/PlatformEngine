005203554fc7496a0e1f16004e0dfd7b440ea318
Thomas B <bocquet.T@gmail.com>
2019-01-30 17:22:12 +0100

    Add unit tests step in CI (#2)
    * add testing step in CI
    
    * fix unit test
    
    * fix unit test bis

---
+++ CI/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo "Run tests on following projects :"
+IFS=$'\n'
+projects=""
+for i in $(cat < "$1"); do
+    echo $i
+    projects+="$SCRIPTPATH/$i"
+done
+
+mono /usr/local/bin/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe $projects
