375f08b33b71dcb461bbfe813818af09b35598c7
Thomas <bocquet.t@gmail.com>
2019-01-30 17:14:16 +0100

    fix unit test

--- CI/test.sh
+++ CI/test.sh
@@ -2,9 +2,10 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "Run tests on following projects :"
 IFS=$'\n'
+projects=""
 for i in $(cat < "$1"); do
-    echo "$i"
-   $projects += "$SCRIPTPATH/$i"
+    echo $i
+    projects+="$SCRIPTPATH/$i"
 done
 
-mono usr/local/bin/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe $projects
+mono /usr/local/bin/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe $projects
