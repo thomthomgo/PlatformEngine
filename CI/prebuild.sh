9a7403f632d230a5e722a836aa3b74cdb3a22f94
Thomas <bocquet.t@gmail.com>
2019-01-30 16:27:57 +0100

    dbg script

--- CI/prebuild.sh
+++ CI/prebuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $unityPath
 mkdir $SCRIPTPATH/../packages
 ln -s ${unityPath}/Editor/Data/Managed/UnityEngine.dll $SCRIPTPATH/../packages
 ls -la $SCRIPTPATH/../packages
